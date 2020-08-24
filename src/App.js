7cc3685aa0239df3fd4140bec46d6862e0590ee0
Anshul Saxena <anshul.saxena@sap.com>
2020-08-25 01:34:57 +0530

    Create App components

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
+import Logo from './components/Logo';
+import ImageLinkForm from './components/ImageLinkForm';
+import Rank from './components/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      { /*
+        <FaceRecognition />
+        */
+      }
+    </div>
+  );
+}
+
+export default App;
