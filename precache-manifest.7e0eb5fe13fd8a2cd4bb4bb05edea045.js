0c4ce55718d871b64772381dd49e8e55f12ffdfc
Anshul Saxena <anshul.saxena@sap.com>
2020-08-26 23:53:41 +0530

    Updates

---
+++ precache-manifest.7e0eb5fe13fd8a2cd4bb4bb05edea045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060069ea353bff369b88f26cf7da7af4",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "cfa2e900c6b8e0d01a49",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "161a384d11640ef0e24c",
+    "url": "/facerecognitionbrain/static/css/main.d3a18bbf.chunk.css"
+  },
+  {
+    "revision": "cfa2e900c6b8e0d01a49",
+    "url": "/facerecognitionbrain/static/js/2.f824017c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.f824017c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161a384d11640ef0e24c",
+    "url": "/facerecognitionbrain/static/js/main.c514dd93.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
